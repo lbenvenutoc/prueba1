676dfcf7037225563dd00676269524c2a0b88126
JAVA-ADV-VS <JAVA-ADV-VS@S220-ST>
2013-11-02 10:33:23 -0500

    Priemr commit

---
+++ src/prueba1/Prueba1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1;
+
+/**
+ *
+ * @author JAVA-ADV-VS
+ */
+public class Prueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
